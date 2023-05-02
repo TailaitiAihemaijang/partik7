bac04eb5d960f20ceb48daa8067dfefdce517283
Bavudun <106803325+abdurahim0201@users.noreply.github.com>
2023-05-02 11:16:22 +0300

    Update ManavKasa.java

--- src/ManavKasa.java
+++ src/ManavKasa.java
@@ -9,16 +9,7 @@ import java.util.Scanner;
  * @Create 5/2/2023 10:40 AM
  * @Version 1.0
  */
-//Java ile kullanıcıların manavdan almış oldukları ürünlerin kilogram
-// değerlerine göre toplam tutarını ekrana yazdıran programı yazın.
-//
-//Meyveler ve KG Fiyatları
-//
-//Armut : 2,14 TL
-//Elma : 3,67 TL
-//Domates : 1,11 TL
-//Muz: 0,95 TL
-//Patlıcan : 5,00 TL
+
 public class ManavKasa {
     public static void main(String[] args){
         double armut =2.14,elma = 3.67,domates =1.11,muz = 0.95,patlican = 5.00;
